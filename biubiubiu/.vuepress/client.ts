3fd81266753ec602a676595462932609126fa054
huamurui <yuntai013@outlook.com>
2023-01-02 10:52:51 +0800

    update normally

---
+++ biubiubiu/.vuepress/client.ts
@@ -0,0 +1,8 @@
+import { defineClientConfig } from "@vuepress/client";
+import Snowfall from "./components/Snowfall.vue";
+
+export default defineClientConfig({
+  enhance: ({ app, router, siteData }) => {
+    app.component("Snowfall", Snowfall);
+  },
+})
